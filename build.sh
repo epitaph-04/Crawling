77e6067e8424e479ca2c7f5b2e09d3679f9485f9
Mahbub Hassan <sm.mahbub.hassan@gmail.com>
2019-01-04 22:30:54 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,2 @@
 dotnet restore
 dotnet build
-dotnet pack --no-build --output nupkgs
-dotnet publish SportApi/SportApi.csproj --framework netcoreapp2.2 -r linux-x64 --self-contained -o publishFolder
